0f58192fd763362d0e75ec4de75736f686cfb0c7
Patrik Jira <patrikjira@gmail.com>
2012-02-22 15:58:55 +0100

    Better devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :"etabliocms_core/users"
-
-  devise_scope :"etabliocms_core/user" do
-    get "login" => "devise/sessions#new"
-    delete "logout" => "devise/sessions#destroy"
-  end
+  devise_for :"etabliocms_core/users", :path => ""
 
   match "/admin", :to => "admin/static#index"
   match "/admin/textile_parser", :to => "admin/static#textile_parser", :as => "textile_parser"
