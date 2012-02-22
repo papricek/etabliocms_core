23359f24f45a0bf9d57de83c09a3916c10f62c99
Patrik Jira <patrikjira@gmail.com>
2012-02-22 10:13:28 +0100

    Shorter login and logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
 
   devise_for :"etabliocms_core/users", :path_names => {:sign_in => "login",
-                                     :sign_out => "logout",
-                                     :registration => "register",
-                                     :password => "forgot-password"}
+                                                       :sign_out => "logout",
+                                                       :registration => "register",
+                                                       :password => "forgot-password"}
+
+  devise_scope :user do
+    get "/login" => "devise/sessions#new"
+    delete "/logout" => "devise/sessions#destroy"
+  end
 
   match "/admin", :to => "admin/static#index"
   match "/admin/textile_parser", :to => "admin/static#textile_parser", :as => "textile_parser"
