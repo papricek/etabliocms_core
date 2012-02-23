1aa8dff322b2eaeaae0abefe2491d0c98fc4d664
Patrik Jira <patrikjira@gmail.com>
2012-02-23 07:50:36 +0100

    czech localization file for core and its initialization

--- lib/etabliocms_core/engine.rb
+++ lib/etabliocms_core/engine.rb
@@ -2,10 +2,14 @@ module EtabliocmsCore
 
   class Engine < Rails::Engine
 
-    initializer "etabliocms_core.load_myapp_instance_data" do |app|
+    initializer "etabliocms_core.initialize" do |app|
       EtabliocmsCore.setup do |config|
         config.app_root = app.root
       end
+    end
+
+    initializer "etabliocms_core.configure_app" do |app|
+      app.config.i18n.default_locale = :cs
     end
 
     initializer "etabliocms_core.load_static_assets" do |app|
