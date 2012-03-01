d54f2b8f140c5583348f2035e40535939ed71a73
Patrik Jira <patrikjira@gmail.com>
2012-03-01 16:39:31 +0100

    Cleanup in helpers

--- app/helpers/etabliocms_core/application_helper.rb
+++ app/helpers/etabliocms_core/application_helper.rb
@@ -5,7 +5,6 @@ module EtabliocmsCore
   module ApplicationHelper
 
     include OutputHelper
-    include PagesHelper
 
     def add_breadcrumb(title, url = nil)
       @breadcrumbs << OpenStruct.new(:title => title, :url => url)
@@ -28,24 +27,6 @@ module EtabliocmsCore
     def body_class
       qualified_controller_name = controller.controller_path.gsub('/', '-')
       "#{qualified_controller_name} #{qualified_controller_name}-#{controller.action_name}"
-    end
-
-    def render_sidebar?
-      return false if params[:action] == 'index' && params[:controller] == 'pages'
-      return false if params[:action] == 'search' && params[:controller] == 'pages'
-      return false if params[:controller] == 'devise/sessions'
-      true
-    end
-
-    def other_locale
-      case I18n.locale.to_s
-        when 'cs'
-          'en'
-        when 'en'
-          'cs'
-        else
-          'cs'
-      end
     end
 
   end
