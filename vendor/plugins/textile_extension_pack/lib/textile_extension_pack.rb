5e9bcfb75ac06c61a1cb8a3d76f5c57848cf7e39
Patrik Jira <patrikjira@gmail.com>
2012-03-11 18:41:16 +0100

    Typo

--- vendor/plugins/textile_extension_pack/lib/textile_extension_pack.rb
+++ vendor/plugins/textile_extension_pack/lib/textile_extension_pack.rb
@@ -21,7 +21,7 @@ module TextileExtensionPack
   module RegularExpressions
     def tags(text)
       text.gsub!(/cleaner./, '<hr class="cleaner" />')
-      text.gsub!(/columnsstart./, '<div class="column">')
+      text.gsub!(/columnstart./, '<div class="column">')
       text.gsub!(/columnend./, '</div>')
       text.gsub!(/decolumnize./, '</div>')
       text.gsub!(/columnize./, '<div class="columnized">')
