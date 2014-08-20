39ba7edb2c9c78f0788858aa251785b34286579e
Jaromír Červenka <jaromir@virt.io>
2014-08-20 20:28:03 +0200

    Update to the latest RoR 3.x gems

--- app/assets/javascripts/admin-core.js
+++ app/assets/javascripts/admin-core.js
@@ -1,6 +1,6 @@
 //= require jquery
 //= require jquery_ujs
-//= require jquery.ui.all
+//= require jquery-ui
 //= require ../../../vendor/assets/javascripts/markitup/jquery.markitup.pack
 //= require ../../../vendor/assets/javascripts/markitup/sets/textile/set
 //= require core
